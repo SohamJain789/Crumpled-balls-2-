17db04e44867050b316f28a4c18383071c928490
SohamJain789 <69146549+SohamJain789@users.noreply.github.com>
2020-09-01 20:39:02 +0530

    Add files via upload

--- Ball.js
+++ Ball.js
@@ -4,26 +4,26 @@ constructor(x,y,radius){
     
     isStatic:false,
     restitution:0.3,
-    friction:0,
+    friction:0.5,
     density:1.2
 }
 this.x=x;
 this.y=y;
 this.radius = radius;
 this.image = loadImage("paper.png");
-this.body = Bodies.circle(this.x,this.y,(this.radius-20)/2,options);
+this.body1 = Matter.Bodies.circle(this.x,this.y,(this.radius-20)/2,options);
 
 
-World.add(world,this.body);
+World.add(world,this.body1);
 
 }
 display(){
-var pos =this.body.position;
+var pos =this.body1.position;
 
 push();
 translate(pos.x,pos.y);
 
-fill(255,0,255);
+fill("white");
 //ellipseMode(RADIUS);
 imageMode(CENTER)
 image(this.image,0,0,this.r,this.r);
--- Dustbin.js
+++ Dustbin.js
@@ -8,7 +8,7 @@ class Dustbin
 		this.dustbinHeight=100;
 		this.wallThickness=20;
 		this.angle=0;
-		this.image = loadImage("dustbingreen.png");	
+		this.image=loadImage("dustbingreen.png");
 		
 		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
 		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
@@ -61,6 +61,10 @@ class Dustbin
 			fill(255)
 			rect(0,0,this.dustbinWidth, this.wallThickness);
 			pop()
+
+			imageMode(CENTER)
+            image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+
 			
 	}
 
